ae22acef5bbb5d1e4adfe582ca9921f9c566b3dd
Federico Aloi <federico.aloi@gmail.com>
2014-12-09 01:11:51 -0300

    Spec para correr samples

---
+++ test/SamplesRunnerSpec.hs
@@ -0,0 +1,42 @@
+module SamplesRunnerSpec where
+
+import Test.Hspec
+import Data.Maybe
+import Data.Map as Map
+import ParserLib
+import LISParser
+import LISCompilation
+import AssemblyOptimization
+import AssemblyExecution
+
+run filename = do code <- readFile filename
+                  let prog = parseLIS $ code
+                      res = execProgram $
+                            optimizeProgram $
+                            compileProgram $
+                            prog
+                   in do return res
+
+parseLIS = fst . (bestFirst lisParser) . lisLexer
+
+running filename = run $ "samples/" ++ filename ++ ".lis"
+shouldHaveResult mvs expectedResult = do result <- mvs; (fromJust.(Map.lookup "return").datamem) result `shouldBe` expectedResult
+
+spec :: Spec
+spec = do
+  describe "Samples" $ do
+    it "factorial" $ do
+      running "factorial" `shouldHaveResult` 720
+
+    it "nToBinary" $ do
+      running "nToBinary" `shouldHaveResult` 11101011
+
+    it "boolTest" $ do
+      running "boolTest" `shouldHaveResult` 1
+
+    it "summatory" $ do
+      running "summatory" `shouldHaveResult` 55
+
+main :: IO ()
+main = hspec spec
+
