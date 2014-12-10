585e6081e8d67da209546f5e329d0e0cc989d574
Federico Aloi <federico.aloi@gmail.com>
2014-12-10 16:32:22 -0300

    Funciona State con MayFail

--- src/StateMonad.hs
+++ src/StateMonad.hs
@@ -15,30 +15,38 @@ module StateMonad (
 
 import MayFail
 
-newtype State s a = State {runState :: s -> (a, s)}
+newtype State s a = State {runState :: s -> MayFail (a, s)}
 
 instance Monad (State s) where
-   return x = State $ \s -> (x, s)
+   return x = State $ \s -> Ok (x, s)
 
    (>>=) m f = State $ \s ->
-      let (a, s') = runState m s
-      in runState (f a) s'
+      let result = runState m s
+      in case result of
+       Ok (a, s') -> runState (f a) s'
+       Raise ex -> Raise ex
 
 -- No se por que pero no puedo usar el constructor desde el test
-state :: (s -> (a, s)) -> State s a
+state :: (s -> MayFail (a, s)) -> State s a
 state = State
 
 evalState :: State s a -> s -> MayFail a
-evalState m = (MayFail.Ok).fst.(runState m)
+evalState m s = let result = runState m s
+              in case result of
+               Ok (a, _) -> Ok a
+               Raise ex -> Raise ex
 
 execState :: State s a -> s -> MayFail s
-execState m = (MayFail.Ok).snd.(runState m)
+execState m s = let result = runState m s
+              in case result of
+               Ok (_, s) -> Ok s
+               Raise ex -> Raise ex
 
 getState :: State s s
-getState = State $ \x -> (x, x)
+getState = State $ \x -> Ok (x, x)
 
 updState :: (s -> s) -> State s ()
-updState f = State $ \s -> ((), f s)
+updState f = State $ \s -> Ok ((), f s)
 
 errState :: Exception -> State s a
-errState ex = error "Not yet implemented"
+errState ex = State $ \_ -> Raise ex
